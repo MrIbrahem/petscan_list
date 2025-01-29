b266b259a7921e0b6fd52565c5e6c50b7be9670e
ibrahem <ibrahem.al-radaei@outlook.com>
2025-01-29 06:02:00 +0300

    .

---
+++ update.sh
@@ -0,0 +1,15 @@
+cd $HOME
+
+rm -rf $HOME/www/python/src
+
+git clone https://github.com/MrIbrahem/petscan_list.git $HOME/www/python/src
+
+source $HOME/www/python/venv/bin/activate
+
+pip install --upgrade pip
+
+pip install -r $HOME/www/python/src/requirements.txt
+
+exit
+
+webservice python3.9 start
