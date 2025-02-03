8bd5a4935eef4739d01dcf39744a05b7a0d6eed0
ibrahem <ibrahem.al-radaei@outlook.com>
2025-02-03 03:58:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 """
+www/python/venv/bin/python3 www/python/src/bot.py
+
 python3 core8/pwb.py petscan_list/bot
 python3 I:\core\bots\new\petscan_list\bot.py
 """
@@ -23,7 +25,7 @@ def start():
             # ---
             for n, x in enumerate(pages):
                 print("_______________")
-                print(f"p: {n}/{len(pages)} title: {x}")
+                print(f"p: {n}/{len(pages)} title: {x} ({wiki=})")
                 # ---
                 one_page(x, wiki)
 
