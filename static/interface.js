c92a6a8ea867cb636b206055da340239a97e4e58
ibrahem <ibrahem.al-radaei@outlook.com>
2025-02-14 04:31:39 +0300

    .

--- static/interface.js
+++ static/interface.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     // ---
     // wait 1 second before doing next
     setTimeout(function () {
-        $('#interface_language_wrapper>form').addClass('w-50');
+        // $('#interface_language_wrapper>form').addClass('w-50');
         $('#interface_language_wrapper>form>a').hide();
         // <option value="ar" selected="">AR</option> change AR to "العربية"
         $('option[value="ar"]').text('العربية');
