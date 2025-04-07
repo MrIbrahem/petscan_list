c0425dd575a60968360f254a9c6747bd058affee
ibrahem <ibrahem.al-radaei@outlook.com>
2025-04-07 05:07:34 +0300

    1

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from PetScanList import one_page, MakeTemplate, valid_wikis, valid_projects, get
 
 app = Flask(__name__)
 
+
 def is_valid_petscan_url(url: str) -> bool:
     """
     Checks if the given URL is a valid PetScan URL.
@@ -11,9 +12,11 @@ def is_valid_petscan_url(url: str) -> bool:
     urls = ["https://petscan.wmflabs.org", "https://petscan.wmcloud.org"]
     return any(url.startswith(u) for u in urls)
 
+
 @app.route("/wikis", methods=["GET"])
 def get_valid_wikis():
     return jsonify(valid_wikis)
+
 
 @app.route("/wiki_pages", methods=["GET"])
 def get_wiki_pages():
@@ -26,14 +29,16 @@ def get_wiki_pages():
     pages = get_all_pages(lang, project)
     return jsonify(pages)
 
+
 @app.route("/pages", methods=["GET"])
 def get_pages():
     project = request.args.get("project")
     lang = request.args.get("lang")
     if lang and project:
         pages = get_all_pages(lang, project, split_by_ns=True)
-        return render_template("pages.html", pages=pages, lang=lang, project=project), 200
+        return render_template("pages.html", pages_list=pages, lang=lang, project=project), 200
     return render_template("pages.html", wikis=valid_projects), 200
+
 
 @app.route("/template", methods=["POST", "GET"])
 def template():
@@ -48,16 +53,17 @@ def template():
         return render_template("template_form.html", url=url, tt="unexpected_error", tt1=str(e)), 400
     return render_template("template_form.html", result=result, url=url)
 
+
 @app.route("/update", methods=["GET"])
 def update():
     title = request.args.get("title")
     wiki = request.args.get("wiki")
     if not title or not wiki:
         return render_template("result.html", result_class="danger", tt="title_or_wiki_required"), 400
-    
+
     encoded_title = quote(title)
     url = f"https://{wiki}/wiki/{encoded_title}"
-    
+
     if wiki not in valid_wikis:
         return render_template("result.html", title=title, url=url, result_class="danger", tt="wiki_not_supported", tt1=wiki), 400
     try:
@@ -73,21 +79,26 @@ def update():
     except Exception as e:
         return render_template("result.html", title=title, url=url, result_class="danger", tt="unexpected_error", tt1=str(e)), 400
 
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", wikis=valid_wikis)
+
 
 @app.route("/tutorials", methods=["GET"])
 def tutorials():
     return render_template("tutorials.html", wikis=valid_wikis)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template("error.html", tt="invalid_url", error=str(e)), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("error.html", tt="unexpected_error", error=str(e)), 500
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=3500)
