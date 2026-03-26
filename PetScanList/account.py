9547d4789176ef307abd754b476d2174b0a34400
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-26 08:05:13 +0300

    Update account.py

--- PetScanList/account.py
+++ PetScanList/account.py
@@ -1,6 +1,9 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+try:
+    load_dotenv()
+except Exception:
+    pass
 
 username = os.getenv("WIKIPEDIA_BOT_USERNAME", "")
 password = os.getenv("WIKIPEDIA_BOT_PASSWORD", "")
